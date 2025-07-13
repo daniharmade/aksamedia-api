5ac9f0186d5ed8e7c6fb0957d05d1f932ce79f0f
Dani Harmade <128836963+daniharmade@users.noreply.github.com>
2025-07-14 00:25:31 +0700

    add: validation method

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -4,6 +4,7 @@ namespace App\Exceptions;
 
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 use Throwable;
+use Symfony\Component\HttpKernel\Exception\MethodNotAllowedHttpException;
 
 class Handler extends ExceptionHandler
 {
@@ -44,5 +45,17 @@ class Handler extends ExceptionHandler
         $this->reportable(function (Throwable $e) {
             //
         });
+    }
+
+    public function render($request, Throwable $exception)
+    {
+        if ($exception instanceof MethodNotAllowedHttpException) {
+            return response()->json([
+                'status' => 'error',
+                'message' => 'Metode tidak diizinkan untuk endpoint ini.',
+            ], 405);
+        }
+
+        return parent::render($request, $exception);
     }
 }
